c508c2cb587ebe2b139f0a61fd27a072c9e5863d
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2013-12-06 02:46:48 +0400

    update

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class User(Base):
         return '<User %r>' % (self.username)
 
 
-class Entries(Base):
+class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
     title = Column(String(120), unique=True)
