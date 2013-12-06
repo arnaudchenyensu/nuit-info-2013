d2dc4a8229ce822f3fd6ad5dab18b945a4bae9d0
Arnaud Chen-yen-su <arnaud.chenyensu@gmail.com>
2013-12-06 09:30:37 +0400

    update

--- database.py
+++ database.py
@@ -10,10 +10,8 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-	"""Initialise the Database."""
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
     import models
-    # Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
